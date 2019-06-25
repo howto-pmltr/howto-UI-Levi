847884ee85493e6136c0c34cb6078c42a5f5a8d6
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-06-25 14:03:33 -0600

    button functionality

---
+++ Header/header.js
@@ -0,0 +1,24 @@
+class NavLink {
+    constructor(domElement) {
+      this.domElement = domElement; 
+      this.expandButton = document.querySelector('.caret-open');
+      this.caretIcons = document.querySelectorAll('.fas');
+      this.expandButton.addEventListener('click', this.expandLinks.bind(this));
+      this.expandButton.addEventListener('click', this.caretFlip.bind(this));
+    }
+  
+    expandLinks() {
+      this.domElement.classList.toggle('nav-link--open');
+    }
+
+    caretFlip() {
+        this.caretIcons.forEach((icon) => icon.classList.toggle('caret-open'));
+    }
+  }
+ 
+  
+  let navLinks = document.querySelectorAll('.nav-link');
+  
+  navLinks.forEach((link) => {
+    return new NavLink(link);
+  });
